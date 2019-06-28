d8dd6454a8f3a4499d77367064e80be65f898a68
Elliot Garbus <elliotg2@cox.net>
2019-06-27 21:09:13 -0700

    Need to fix popup error

--- drivepathsetting.py
+++ drivepathsetting.py
@@ -1,4 +1,4 @@
-from kivy.uix.settings import SettingItem, SettingSpacer
+from kivy.uix.settings import SettingItem, SettingPath
 from kivy.uix.popup import Popup
 from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import StringProperty, ObjectProperty, BooleanProperty
@@ -47,11 +47,11 @@ KV = """
                 disabled: 0==len( fc.selection )
                 on_release:
                     root.action(fc.selection[0])
-                    root.dismiss()
+                    root.dismiss_action()
             Button:
                 text: 'Cancel'
                 on_release: 
-                    root.dismiss()
+                    root.dismiss_action()
                     
                     
 """
@@ -63,98 +63,38 @@ setting_panel_kv = """
         pos: root.pos
         font_size: '15sp'
 """
-Builder.load_string(setting_panel_kv)
+# Builder.load_string(setting_panel_kv)
+
 
 class DirectoryDialog(Popup):
     path = StringProperty()    # The path of the dir to view
     action = ObjectProperty()  # action to execute on ok
     instance = ObjectProperty()
+    dismiss_action = ObjectProperty()  # call the _dismiss
 
     def __init__(self, **kwargs):
         self.path = kwargs['path']
         self.action = kwargs['action']
         self.instance = kwargs['instance']
+        self.dismiss_action= kwargs['dismiss_action']
         super().__init__(**kwargs)
 
 
-class SettingDrivePath(SettingItem):
-    # def __init__(self, **kwargs):
-    #     super().__init__(**kwargs)
+class SettingDrivePath(SettingPath):
 
-    original = False   # If original is True, then use code from settingspath, else use my code.
-    if original:
-        popup = ObjectProperty(None, allownone=True)
-        textinput = ObjectProperty(None)
-        show_hidden = BooleanProperty(False)
-        dirselect = BooleanProperty(True)
+    def _validate(self, instance):
+        pass
 
-        def on_panel(self, instance, value):
-            if value is None:
-                return
-            self.fbind('on_release', self._create_popup)
+    def set_new_path(self, new_path):
+        self._dismiss()
+        self.value = new_path
 
-        def _dismiss(self, *largs):
-            if self.textinput:
-                self.textinput.focus = False
-            if self.popup:
-                self.popup.dismiss()
-            self.popup = None
-
-        def _validate(self, instance):
-            self._dismiss()
-            value = self.textinput.selection
-
-            if not value:
-                return
-
-            self.value = os.path.realpath(value[0])
-            print(f'path: {self.value}, textinput: {self.textinput.selection}')
-
-        def _create_popup(self, instance):
-            # create popup layout
-            content = BoxLayout(orientation='vertical', spacing=5)
-            popup_width = dp(500)
-            self.popup = popup = Popup(
-                title=self.title, content=content, size_hint=(None, 0.9),
-                width=popup_width)
-
-            # create the filechooser
-            initial_path = self.value or os.getcwd()
-            self.textinput = textinput = FileChooserListView(
-                path=initial_path, size_hint=(1, 1),
-                dirselect=self.dirselect, show_hidden=self.show_hidden)
-            textinput.bind(on_path=self._validate)
-
-            # construct the content
-            content.add_widget(textinput)
-            content.add_widget(SettingSpacer())
-
-            # 2 buttons are created for accept or cancel the current value
-            btnlayout = BoxLayout(size_hint_y=None, height='50dp', spacing='5dp')
-            btn = Button(text='Ok')
-            btn.bind(on_release=self._validate)
-            btnlayout.add_widget(btn)
-            btn = Button(text='Cancel')
-            btn.bind(on_release=self._dismiss)
-            btnlayout.add_widget(btn)
-            content.add_widget(btnlayout)
-
-            # all done, open the popup !
-            popup.open()
-    else:    #  My code
-        def on_panel(self, instance, value):
-            if value is None:
-                return
-            self.fbind('on_release', self._create_popup)
-
-        def set_new_path(self, new_path):
-            self.value = new_path
-
-        def _create_popup(self, instance):
-            content = Builder.load_string(KV)
-            initial_path = self.value or os.getcwd()
-            popup = DirectoryDialog(title=self.title,
-                                    path=initial_path,
-                                    action=self.set_new_path,
-                                    instance=instance)
-            popup.open()
+    def _create_popup(self, instance):
+        Builder.load_string(KV)
+        initial_path = self.value or os.getcwd()
+        self.popup = popup = DirectoryDialog(title=self.title,
+                                             path=initial_path,
+                                             action=self.set_new_path,
+                                             dismiss_action=self._dismiss,
+                                             instance=instance)
+        popup.open()
